1efac4d67ed1fcd533d2173110b9df2ee7b9817f
Agustin Canavari <24359306+agustincanavari@users.noreply.github.com>
2024-02-22 15:26:57 -0300

    x

--- main.go
+++ main.go
@@ -5,19 +5,29 @@ import (
 	"os"
 
 	"github.com/adshao/go-binance/v2"
+	"github.com/adshao/go-binance/v2/delivery"
 	"github.com/joho/godotenv"
 )
 
-func main() {
+func loadEnv() (string, string) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 	apiKey := os.Getenv("API_KEY")
 	secretKey := os.Getenv("SECRET_KEY")
+	return apiKey, secretKey
+}
 
+func createClients(apiKey, secretKey string) (*binance.Client, *delivery.Client) {
 	spotClient := binance.NewClient(apiKey, secretKey)
 	deliveryClient := binance.NewDeliveryClient(apiKey, secretKey)
+	return spotClient, deliveryClient
+}
+
+func main() {
+	apiKey, secretKey := loadEnv()
+	spotClient, deliveryClient := createClients(apiKey, secretKey)
 
 	deliverableFutures := fetchFutures(deliveryClient)
 	calculators := generateRateCalculators(deliverableFutures)
